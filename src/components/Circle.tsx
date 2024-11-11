d7772d780edddf7de80c5252b79074db39850803
Dmitry Komalov <dmitrykomalov@gmail.com>
2024-11-11 23:27:59 +0000

    cleanup ball svg import

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect, useRef } from "react";
 import clsx from "clsx";
 import "../App.css";
 
-import ball from "../assets/ball.svg";
-
 import { Coords } from "../constants";
 
 type Background = string;
